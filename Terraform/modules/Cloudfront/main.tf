9f4d8bef7e0e7eca44cfeb35b75fcef8feaf33ea
MopsesDevopses <63007614+MopsesDevopses@users.noreply.github.com>
2020-04-13 11:43:08 +0300

    Update main.tf

--- Terraform/modules/Cloudfront/main.tf
+++ Terraform/modules/Cloudfront/main.tf
@@ -15,7 +15,7 @@ resource "aws_s3_bucket_policy" "s3_bucket_policy" {
           "Sid": "1",
           "Effect": "Allow",
           "Principal": {
-              "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_distribution.s3_distribution.id}"
+              "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity ${aws_cloudfront_origin_access_identity.origin_access_identity.id}"
           },
           "Action": "s3:GetObject",
           "Resource": "${aws_s3_bucket.s3.arn}/*"
