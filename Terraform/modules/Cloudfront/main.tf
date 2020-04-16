a534624e256450ca9de3b1626c10385069d57a2d
MopsesDevopses <63007614+MopsesDevopses@users.noreply.github.com>
2020-04-16 13:17:42 +0300

    Update main.tf

--- Terraform/modules/Cloudfront/main.tf
+++ Terraform/modules/Cloudfront/main.tf
@@ -1,6 +1,11 @@
 resource "aws_s3_bucket" "s3" {
   bucket = "${var.s3_bucket}"
-  acl    = "private"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+    }
 }
 
 resource "aws_s3_bucket_policy" "s3_bucket_policy" {
